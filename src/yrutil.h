1e4466d7206cbc5d8c46c4e6570b1b42ad71262c
Conor Hughes <conorh@conorh.net>
2018-11-19 22:06:05 -0800

    mark __yr_runtime_assert_fail__ as noreturn

--- src/yrutil.h
+++ src/yrutil.h
@@ -1,9 +1,11 @@
 #ifndef YACHTROCK_YRUTIL_H
 #define YACHTROCK_YRUTIL_H
 
+#include <stdnoreturn.h>
+
 extern char *yr_strdup(const char *in);
 
-extern void __yr_runtime_assert_fail__(char *fmt, ...);
+extern noreturn void __yr_runtime_assert_fail__(char *fmt, ...);
 
 #define YR_RUNTIME_ASSERT(test, ...) do {       \
     if ( !(test) ) {                            \
