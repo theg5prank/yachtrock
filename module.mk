315ccf1c2364a476adc79b14242160454e84d1d5
Conor Hughes <conorh@conorh.net>
2018-11-25 19:15:20 -0800

    Factor test makefile stuff into its own file

--- module.mk
+++ module.mk
@@ -11,7 +11,6 @@ endif
 LIBYACHTROCK_ARNAME := libyachtrock.a
 
 PRODUCTS += $(LIBYACHTROCK_DYLIBNAME) $(LIBYACHTROCK_ARNAME)
-TESTS += test_libyachtrock
 
 MODULE_CLEANS += clean_libyachtrock
 
@@ -23,46 +22,15 @@ LIBYACHTROCK_SRC := $(LIBYACHTROCK_STATIC_SRC) $(LIBYACHTROCK_GENERATED_SRC)
 LIBYACHTROCK_HEADER_SOURCES := $(wildcard $(LIBYACHTROCK_DIR)public_headers/yachtrock/*.h)
 LIBYACHTROCK_HEADER_INSTALLED_FILES := $(subst $(LIBYACHTROCK_DIR)public_headers,$(PREFIX)/include,$(LIBYACHTROCK_HEADER_SOURCES))
 
-LIBYACHTROCK_TESTSRC := basic_tests.c result_store_tests.c assertion_tests.c testcase_tests.c dummy_module.c run_under_store_tests.c
-LIBYACHTROCK_TESTSRC := $(patsubst %,$(LIBYACHTROCK_DIR)test/%,$(LIBYACHTROCK_TESTSRC))
 CSRC += $(LIBYACHTROCK_SRC)
-CSRC += $(LIBYACHTROCK_TESTSRC)
 LIBYACHTROCK_OBJ = $(patsubst %.c,%.o,$(filter %.c,$(LIBYACHTROCK_SRC)))
-LIBYACHTROCK_TESTOBJ = $(patsubst %.c,%.o,$(filter %.c,$(LIBYACHTROCK_TESTSRC)))
-LIBYACHTROCK_TESTS = $(patsubst %.o,%,$(LIBYACHTROCK_TESTOBJ))
 LIBYACHTROCK_LINKS = -ldl
-LIBYACHTROCK_TESTSUPPORT =
+
+include $(LIBYACHTROCK_DIR)module_tests.mk
 
 $(LIBYACHTROCK_STATIC_SRC): $(LIBYACHTROCK_GENERATED_SRC)
 
 $(LIBYACHTROCK_OBJ): CFLAGS += -fPIC -D_POSIX_C_SOURCE=200809L
-$(LIBYACHTROCK_TESTOBJ): CFLAGS += -D_POSIX_C_SOURCE=200809L
-
-test_libyachtrock: test_libyachtrock_basic test_libyachtrock_result_store test_libyachtrock_assertion test_libyachtrock_testcase test_libyachtrock_run_under_store
-
-test_libyachtrock_%: libyachtrock_%_tests_success
-	true
-
-libyachtrock_%_tests_success: $(LIBYACHTROCK_DIR)test/%_tests
-	./$<
-
-$(LIBYACHTROCK_DIR)test/dummy_module.o: CFLAGS += -fPIC
-ifeq ($(UNAME_S),Darwin)
-$(LIBYACHTROCK_DIR)test/dummy_module.dylib: $(LIBYACHTROCK_DIR)test/dummy_module.o $(LIBYACHTROCK_ARNAME)
-	$(CC) -dynamiclib $^ -o $@
-else
-$(LIBYACHTROCK_DIR)test/dummy_module.dylib: $(LIBYACHTROCK_DIR)test/dummy_module.o $(LIBYACHTROCK_ARNAME)
-	$(CC) -shared $^ -o $@
-endif
-libyachtrock_testcase_tests_success: $(LIBYACHTROCK_DIR)test/testcase_tests $(LIBYACHTROCK_DIR)test/dummy_module.dylib
-	$^
-LIBYACHTROCK_TESTSUPPORT += $(LIBYACHTROCK_DIR)test/dummy_module.dylib
-
-$(LIBYACHTROCK_DIR)test/%_tests: $(LIBYACHTROCK_DIR)test/%_tests.o $(LIBYACHTROCK_ARNAME)
-	$(CC) $^ $(LIBYACHTROCK_LINKS) -o $@
-
-# without this the test binaries themselves are considered intermediaries and are removed
-.SECONDARY: $(LIBYACHTROCK_TESTS)
 
 $(LIBYACHTROCK_ARNAME): $(LIBYACHTROCK_OBJ)
 	ar rc $@ $(LIBYACHTROCK_OBJ)
@@ -80,9 +48,6 @@ clean_libyachtrock:
 	rm -f $(LIBYACHTROCK_ARNAME)
 	rm -f $(LIBYACHTROCK_DYLIBNAME)
 	rm -f $(LIBYACHTROCK_OBJ)
-	rm -f $(LIBYACHTROCK_TESTOBJ)
-	rm -f $(LIBYACHTROCK_TESTS)
-	rm -f $(LIBYACHTROCK_TESTSUPPORT)
 	rm -f $(LIBYACHTROCK_GENERATED_SRC)
 
 install: install_libyachtrock_dylib install_libyachtrock_headers
---
+++ module_tests.mk
@@ -0,0 +1,44 @@
+TESTS += test_libyachtrock
+
+LIBYACHTROCK_TESTSRC := basic_tests.c result_store_tests.c assertion_tests.c testcase_tests.c dummy_module.c run_under_store_tests.c
+LIBYACHTROCK_TESTSRC := $(patsubst %,$(LIBYACHTROCK_DIR)test/%,$(LIBYACHTROCK_TESTSRC))
+CSRC += $(LIBYACHTROCK_TESTSRC)
+LIBYACHTROCK_TESTOBJ = $(patsubst %.c,%.o,$(filter %.c,$(LIBYACHTROCK_TESTSRC)))
+LIBYACHTROCK_TESTS = $(patsubst %.o,%,$(LIBYACHTROCK_TESTOBJ))
+LIBYACHTROCK_TESTSUPPORT =
+
+$(LIBYACHTROCK_TESTOBJ): CFLAGS += -D_POSIX_C_SOURCE=200809L
+
+clean_libyachtrock_tests:
+	rm -f $(LIBYACHTROCK_TESTOBJ)
+	rm -f $(LIBYACHTROCK_TESTS)
+	rm -f $(LIBYACHTROCK_TESTSUPPORT)
+
+clean_libyachtrock: clean_libyachtrock_tests
+
+test_libyachtrock: test_libyachtrock_basic test_libyachtrock_result_store test_libyachtrock_assertion test_libyachtrock_testcase test_libyachtrock_run_under_store
+
+test_libyachtrock_%: libyachtrock_%_tests_success
+	true
+
+libyachtrock_%_tests_success: $(LIBYACHTROCK_DIR)test/%_tests
+	./$<
+
+$(LIBYACHTROCK_DIR)test/dummy_module.o: CFLAGS += -fPIC
+ifeq ($(UNAME_S),Darwin)
+$(LIBYACHTROCK_DIR)test/dummy_module.dylib: $(LIBYACHTROCK_DIR)test/dummy_module.o $(LIBYACHTROCK_ARNAME)
+	$(CC) -dynamiclib $^ -o $@
+else
+$(LIBYACHTROCK_DIR)test/dummy_module.dylib: $(LIBYACHTROCK_DIR)test/dummy_module.o $(LIBYACHTROCK_ARNAME)
+	$(CC) -shared $^ -o $@
+endif
+libyachtrock_testcase_tests_success: $(LIBYACHTROCK_DIR)test/testcase_tests $(LIBYACHTROCK_DIR)test/dummy_module.dylib
+	$^
+LIBYACHTROCK_TESTSUPPORT += $(LIBYACHTROCK_DIR)test/dummy_module.dylib
+
+$(LIBYACHTROCK_DIR)test/%_tests: $(LIBYACHTROCK_DIR)test/%_tests.o $(LIBYACHTROCK_ARNAME)
+	$(CC) $^ $(LIBYACHTROCK_LINKS) -o $@
+
+# without this the test binaries themselves are considered intermediaries and are removed
+.SECONDARY: $(LIBYACHTROCK_TESTS)
+
