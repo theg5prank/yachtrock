f81cb16fcdb9d61180ac5ed6aeffb1be38703844
Conor Hughes <conorh@conorh.net>
2019-03-05 10:09:17 -0800

    C++ fixes to assert macros

--- public_headers/yachtrock/assert.h
+++ public_headers/yachtrock/assert.h
@@ -23,56 +23,56 @@
 #define YR_ASSERT_EQUAL(val1, val2, ...) do {                           \
     if ( (val1) != (val2) ) {                                           \
       const char *__assertion_desc = #val1 " == " #val2;                \
-      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, ""__VA_ARGS__); \
+      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, "" __VA_ARGS__); \
     }                                                                   \
   } while (0)
 
 #define YR_ASSERT_STRINGS_EQUAL(val1, val2, ...) do {                   \
     if ( strcmp((val1), (val2)) != 0 ) {                                \
       const char *__assertion_desc = "strcmp("#val1 ", " #val2 ") == 0"; \
-      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, ""__VA_ARGS__); \
+      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, "" __VA_ARGS__); \
     }                                                                   \
   } while (0)
 
 #define YR_ASSERT_STRINGS_NOT_EQUAL(val1, val2, ...) do {               \
     if ( strcmp((val1), (val2)) == 0 ) {                                \
       const char *__assertion_desc = "strcmp("#val1 ", " #val2 ") != 0"; \
-      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, ""__VA_ARGS__); \
+      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, "" __VA_ARGS__); \
     }                                                                   \
   } while (0)
 
 #define YR_ASSERT_NOT_EQUAL(val1, val2, ...) do {                       \
     if ( (val1) == (val2) ) {                                           \
       const char *__assertion_desc = #val1 " != " #val2;                \
-      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, ""__VA_ARGS__); \
+      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, "" __VA_ARGS__); \
     }                                                                   \
   } while (0)
 
 #define YR_ASSERT_LESS_THAN(val1, val2, ...) do {                       \
     if ( !((val1) < (val2)) ) {                                         \
       const char *__assertion_desc = #val1 " < " #val2;                 \
-      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, ""__VA_ARGS__); \
+      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, "" __VA_ARGS__); \
     }                                                                   \
   } while (0)
 
 #define YR_ASSERT_GREATER_THAN(val1, val2, ...) do {                    \
     if ( !((val1) > (val2)) ) {                                         \
       const char *__assertion_desc = #val1 " > " #val2;                 \
-      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, ""__VA_ARGS__); \
+      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, "" __VA_ARGS__); \
     }                                                                   \
   } while (0)
 
 #define YR_ASSERT_LESS_THAN_OR_EQUAL(val1, val2, ...) do {              \
     if ( !((val1) <= (val2)) ) {                                        \
       const char *__assertion_desc = #val1 " <= " #val2;                \
-      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, ""__VA_ARGS__); \
+      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, "" __VA_ARGS__); \
     }                                                                   \
   } while (0)
 
 #define YR_ASSERT_GREATER_THAN_OR_EQUAL(val1, val2, ...) do {           \
     if ( !((val1) >= (val2)) ) {                                        \
       const char *__assertion_desc = #val1 " >= " #val2;                \
-      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, ""__VA_ARGS__); \
+      yr_fail_assertion(__assertion_desc, __FILE__, __LINE__, __FUNCTION__, "" __VA_ARGS__); \
     }                                                                   \
   } while (0)
 
